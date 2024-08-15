0019af35443a8ef994b97334fed89db67c351966
TheR3dWizard <sreeraghavan2016@gmail.com>
2024-08-15 15:31:18 +0530

    exp3 server

---
+++ exp3/server.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main(){
+    int serv_fd, new_sock, valread;
+    struct sockaddr_in addr;
+    int opt = 1;
+    socklen_t addrlen = sizeof(addr);
+    char buffer[BUFFER_SIZE] = {0};
+
+    if(!(serv_fd = socket(AF_INET,SOCK_STREAM,0))){
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Waiting for incoming message...\n\n");
+
+    if(setsockopt(serv_fd,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt))){
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+    if(bind(serv_fd,(struct sockaddr *)&addr,addrlen) < 0){
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if(listen(serv_fd,3) <0){
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+        if((new_sock = accept(serv_fd,(struct sockaddr *)&addr,addrlen)) < 0){
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        memset(buffer,0,BUFFER_SIZE);
+    }
+}
